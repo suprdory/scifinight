47eced505ca1bcd373facfa9b91417a4d26f8772
suprdory <suprdory@gmail.com>
2024-10-25 10:14:35 +0100

    manual yotso film add

--- pyfi/add_new_films.py
+++ pyfi/add_new_films.py
@@ -1,19 +1,22 @@
-#%%
+# %%
 import pandas as pd
-import json, requests
+import json
+import requests
 import numpy as np
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 omdb_api_key = config['omdb_api_key']
-#%%
+# %%
 
 # df_old=pd.read_pickle('df_scfi_db_181024_posters.pkl')
-df_old = pd.read_csv('scifi_data_251024.csv').iloc[:,1:]
+df_old = pd.read_csv('scifi_data_281024.csv').iloc[:, 1:]
 # %%
-newData=pd.read_csv('to_add.csv')
+newData = pd.read_csv('to_add.csv')
 
 # %%
+
+
 def get_scores(omdb_ratings):
     sources = ['Internet Movie Database', 'Rotten Tomatoes', 'Metacritic']
 
@@ -42,12 +45,14 @@ def get_scores(omdb_ratings):
     Metacritic = get_score(omdb_ratings, 'Metacritic')
     return (IMDb, RT, Metacritic)
 
+
 def get_omdb_imdb(imdb_id):
     urlbase = f'http://www.omdbapi.com/?apikey={omdb_api_key}='
     url = urlbase+imdb_id
     response = requests.get(url)
     dict = json.loads(response.text)
     return dict
+
 
 def get_tmdb_imdb(imdb_id):
     url = f"https://api.themoviedb.org/3/find/{imdb_id}?external_source=imdb_id"
@@ -62,8 +67,10 @@ def get_tmdb_imdb(imdb_id):
         return out[0]
     else:
         return None
-#%%
-def buildFilmDat(imdbID,season):
+# %%
+
+
+def buildFilmDat(imdbID, season):
     omdbDat = get_omdb_imdb(imdbID)
     tmdbDat = get_tmdb_imdb(imdbID)
 
@@ -71,53 +78,77 @@ def buildFilmDat(imdbID,season):
     runtime = int(omdbDat['Runtime'][:-4])
 
     boStr = omdbDat['BoxOffice']
-    if boStr=='N/A':
-        boxoffice=None
+    if boStr == 'N/A':
+        boxoffice = None
     else:
-        boxoffice=omdbDat['BoxOffice'][1:].replace(',','')
+        boxoffice = omdbDat['BoxOffice'][1:].replace(',', '')
 
-    IMDb,RT,Metacritic=get_scores(omdbDat['Ratings'])
+    IMDb, RT, Metacritic = get_scores(omdbDat['Ratings'])
 
-    fields=['Title', 'imdbID', 'Year', 'Rated', 'Director',
-            'Actors', 'Language', 'Plot',]
-    out={}
+    fields = ['Title', 'imdbID', 'Year', 'Rated', 'Director',
+              'Actors', 'Language', 'Plot',]
+    out = {}
     for field in fields:
-        out[field]=omdbDat[field]
+        out[field] = omdbDat[field]
 
-    out['IMDb_link']=imdb_link
-    out['Runtime']=runtime
-    out['BoxOffice']= boxoffice
+    out['IMDb_link'] = imdb_link
+    out['Runtime'] = runtime
+    out['BoxOffice'] = boxoffice
     out['IMDb'] = IMDb
     out['RT'] = RT
     out['Meta'] = Metacritic
-    out['poster_path']=tmdbDat['poster_path']
+    out['poster_path'] = tmdbDat['poster_path']
     out['backdrop_path'] = tmdbDat['backdrop_path']
-    out['Watched']= ''
-    out['Season']=season
-    return out,omdbDat,tmdbDat
+    out['Watched'] = ''
+    out['Season'] = season
+    return out, omdbDat, tmdbDat
 
 # imdbID = newData.loc[1, 'imdbID']
 
-filmDats=[]
-for i,film in newData.iterrows():
-    filmDat,omdbDat,tmdbDat=buildFilmDat(film['imdbID'],film['Season'])
-    filmDats.append(filmDat)
-    print(film['Title'],omdbDat['Title'])
 
-df_new=pd.DataFrame(filmDats)
+filmDats = []
+for i, film in newData.iterrows():
+    filmDat, omdbDat, tmdbDat = buildFilmDat(film['imdbID'], film['Season'])
+    filmDats.append(filmDat)
+    print(film['Title'], omdbDat['Title'])
+
+df_new = pd.DataFrame(filmDats)
 
 # %%
-df=pd.concat((df_new,df_old))
-df=df.reset_index(drop=True)
+# get_tmdb_imdb("tt0142001")
+# film = {
+#     'Title': "The Year of the Sex Olympics",
+#     'Year': 1968,
+#     'imdbID': "tt0142001",
+#     'Rated': "15",
+#     'Director': "Michael Elliott",
+#     'Actors': "Leonard Rossiter, Suzanne Neve, Tony Vogel, Brian Cox",
+#     'Language': "English",
+#     'Plot': 'Influenced by concerns about overpopulation, the counterculture of the 1960s and the societal effects of television, the play depicts a world of the future where a small elite control the media, keeping the lower classes docile by serving them an endless diet of lowest common denominator programmes and pornography. The play concentrates on an idea the programme controllers have for a new programme which will follow the trials and tribulations of a group of people left to fend for themselves on a remote island. In this respect, the play is often cited as having anticipated the craze for reality television.',
+#     'IMDb_link': "https://www.imdb.com/title/tt0142001",
+#     'Runtime':105,
+#     'BoxOffice':None,
+#     'IMDb':7.0,
+#     'RT':None,
+#     'Meta':None,
+#     'poster_path': '/ztJENDWvfNA7jQiuVTqBm5CLyZk.jpg',
+#     'backdrop_path': '/x776jj9rOKIFCNAIATagE992BbT.jpg',
+#     'Watched':True,
+#     'Season': 1,
+# }
+# df_new=pd.DataFrame([film])
+# %%
+df = pd.concat((df_new, df_old))
+df = df.reset_index(drop=True)
 # df=df.iloc[:,:-2]
-df=df.sort_values("Title")
-df.Watched=df.Watched==1
-#%%
+df = df.sort_values("Title")
+df.Watched = df.Watched == 1
+# %%
 # df=df_old.sort_values('Title')
 # df.loc[df.Title=="Innerspace","Season"]=10
-#%%
+# %%
 
-df.to_csv('scifi_data_261024.csv')
+df.to_csv('scifi_data_281024.csv')
 # df.to_json('films.json', orient='records', indent=4)
 df.to_json('../films.json', orient='records', indent=4)
 
